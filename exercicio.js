ee2065a88ecdc634a16590546c07527976753257
carolinecorreia <carolinecorreiacandido@gmail.com>
2022-05-31 15:02:21 -0300

    exercicio js

---
+++ exercicio.js
@@ -0,0 +1,11 @@
+// Exercício proposto
+
+const nome = "Caroline Correia";
+const idade = 23;
+const frase1 = "Você é o único representante do seu sonho na face da Terra, se isso não fizer você correr chapa, eu não sei o que vai!";
+const nacionalidade = "Brasileira";
+let verdadeiro = true;
+let falso = false;
+
+console.log ("Meu nome é ${nome}, sou ${nacionalidade}, tenho ${idade} anos.");
+console.log ("Já diz Emicida ${frase1}: ${verdadeiro}")
